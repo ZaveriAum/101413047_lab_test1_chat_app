83c903e6fe2a06ad5b791d47fe30f1d1c652dadc
Aum Zaveri <aumzaveri06@gmail.com>
2025-02-05 17:57:03 -0500

    Private chat completely implemented

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const socketio = require('socket.io')
 const mongoose = require('mongoose')
-let authRoutes = require('./routes/authRoutes')// getting the user routes
+let authRoutes = require('./routes/authRoutes')
 const app = express()
+const PrivateMessage = require('./models/PrivateMessage')
 
 const server = app.listen(3000, () => {
     console.log(`Socket server running at http://localhost:3000/`)
@@ -15,10 +16,12 @@ mongoose.connect("mongodb://localhost:27017/chat_app_db")
         console.log("Error: " + err)
     })
 
-const socketIds = [];
-
 const io = socketio(server)
+
+let onlineUsers = {};
+
 // const admin = io.of('/')
+app.use('/v1/auth', authRoutes)
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/views/home.html');
@@ -31,45 +34,72 @@ app.get('/private-chat', (req, res) => {
 app.get('/group-chat', (req, res) => {
     res.sendFile(__dirname + '/views/group_chat.html')
 })
-app.use('/v1/auth', authRoutes)
 
 
 io.on('connection', (socket) => {
-    console.log(`New Socket connection: ${socket.id}`)
-    socketIds.push(socket.id)
-    socket.on('disconnect', (reason) => {
-        socketIds.pop(socket.id)
-        console.log(`Client disconnected ${socket.id} : ${reason}`)
-    })
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on('register_user', (username) => {
+        onlineUsers[username] = socket.id;
+        console.log(`User Registered: ${username} -> ${socket.id}`);
+    });
 
     socket.on('typing', (data) => {
-        io.to(data.to).emit('typing', data)
-    })
+        const recipientSocket = onlineUsers[data.to];
+        if (recipientSocket) {
+            io.to(recipientSocket).emit('typing', { from_user: data.from_user });
+        }
+    });
 
     socket.on('not_typing', (data) => {
-        io.to(data.to).emit('not_typing', data)
-    })
+        const recipientSocket = onlineUsers[data.to];
+        if (recipientSocket) {
+            io.to(recipientSocket).emit('not_typing', { from_user: data.from_user });
+        }
+    });
+
+    socket.on('private_message', async (data) => {
+        const { from_user, to_user, message } = data;
     
+        const newMessage = new PrivateMessage({ from_user, to_user, message });
+        await newMessage.save();
+    
+        const recipientSocket = onlineUsers[to_user];
+        const senderSocket = onlineUsers[from_user];
+    
+        if (recipientSocket) {
+            io.to(recipientSocket).emit('private_message', { from_user, message, date_sent: newMessage.date_sent });
+        }
+    
+        if (senderSocket) {
+            io.to(senderSocket).emit('private_message', { from_user, message, date_sent: newMessage.date_sent });
+        }
+    });
+    
+
+    socket.on('get_chat_history', async ({ from_user, to_user }) => {
+        const messages = await PrivateMessage.find({
+            $or: [
+                { from_user, to_user },
+                { from_user: to_user, to_user: from_user }
+            ]
+        }).sort({ date_sent: 1 });
+
+        socket.emit('chat_history', messages);
+    });
+
     socket.on('group_typing', (data) => {
         io.to(data.group_name).emit('group_typing', data)
     })
-
+    
     socket.on('group_not_typing', (data) => {
         io.to(data.group_name).emit('group_not_typing', data)
     })
-
-    socket.on('message', (data) => {
-        socket.send('Hello from Server')
-    })
-
-    socket.on('chat_message', (data) => {
-        io.to(data.to).emit('chat_message', data)
-    })
-
+    
     socket.on('group_message', (data) =>{
         io.to(data.group_name).emit('group_message', data)
     })
-
+    
     socket.on('join_group', (groupName) =>{
         socket.join(groupName)
     })
@@ -77,6 +107,14 @@ io.on('connection', (socket) => {
      socket.on('leave_group', (groupName) =>{
         socket.leave(groupName)
     })
-    console.log(socketIds)
+    
+    socket.on('disconnect', () => {
+        console.log(`User Disconnected: ${socket.id}`);
+        Object.keys(onlineUsers).forEach(username => {
+            if (onlineUsers[username] === socket.id) delete onlineUsers[username];
+        });
+    });
+
+    console.log(onlineUsers)
 
 })
